01ba3b1cb9eea6a5f4956f58648a0f17dd9da356
Miguel Acosta <103863092+Acostamiguel-04@users.noreply.github.com>
2023-06-12 15:36:40 -0300

    Python
    Trabajo terminado

---
+++ Ejercicio2.py
@@ -0,0 +1,8 @@
+## Modifica la variable del anterior ejercicio en la consola de python y después muestrala por consola para ver la modificación de la variable.
+
+ 
+
+a="Hola Mundo!"
+
+a="Mi nombre es Miguel Acosta"
+print(a)
